304a1b8456b5f2675318b589a216148e1df8741a
Valentin <valentin@mediamonks.com>
2016-01-07 17:54:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * @function getRandomValueInRange
+ * @param {float} min
+ * @param {float} max
+ * @param {boolean} [round = false]
+ * @returns {float|int}
+ */
+export function getRandomValueInRange(min, max, round = false) {
+  let val = Math.random() * (max - min) + min;
+  return round ? Math.round(val) : val;
+}
+
+/**
+ * @function mapValueToRange
+ * @param {float} val
+ * @param {float} inMin
+ * @param {float} inMax
+ * @param {float} outMin
+ * @param {float} outMax
+ * @returns {float}
+ */
+export function mapValueToRange(val, inMin, inMax, outMin, outMax) {
+  if(val < inMin) {
+    return outMin;
+  }
+
+  if(val > inMax) {
+    return outMax;
+  }
+
+  return (val - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
+}
+
